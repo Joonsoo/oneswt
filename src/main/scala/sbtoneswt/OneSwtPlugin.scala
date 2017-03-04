16a3ebeb6abd6bff028fdc6db3310b951c6e5547
Joonsoo <joonsoo@giyeok.com>
2017-03-04 21:53:58 +0900

    Working on sbt oneswt plugin

--- src/main/scala/sbtoneswt/OneSwtPlugin.scala
+++ src/main/scala/sbtoneswt/OneSwtPlugin.scala
@@ -1,6 +1,7 @@
 package sbtoneswt
 
 import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.InputStream
 import java.io.OutputStream
@@ -11,7 +12,6 @@ import java.util.jar.JarOutputStream
 import java.util.jar.Manifest
 import java.util.zip.ZipEntry
 import sbt.Def
-import sbt.Def
 import sbt.Keys._
 import sbt._
 
@@ -20,6 +20,7 @@ import sbt._
 object OneSwtPlugin extends AutoPlugin {
     object autoImport {
         val oneswtAssembly: TaskKey[File] = taskKey[File]("Create a fat jar including swt.")
+        val oneswtResolver: TaskKey[Resolver] = taskKey[Resolver]("Where to resolve swt jars")
 
         val oneswtAssemblyJarName: TaskKey[String] = taskKey[String]("jar name")
         val oneswtAssemblyDefaultJarName: TaskKey[String] = taskKey[String]("jar name")
@@ -28,6 +29,7 @@ object OneSwtPlugin extends AutoPlugin {
         // default values for the tasks and settings
         lazy val baseOneSwtSettings: Seq[Def.Setting[_]] = Seq(
             oneswtAssembly := OneSwt.assemblyTask(oneswtAssembly).value,
+            oneswtResolver in oneswtAssembly := "Local Maven Repository" at "file:///C:/home/joonsoo/workspace/oneswt/maven",
             oneswtAssemblyJarName in oneswtAssembly := ((oneswtAssemblyJarName in oneswtAssembly) or (oneswtAssemblyDefaultJarName in oneswtAssembly)).value,
             oneswtAssemblyDefaultJarName in oneswtAssembly := { name.value + "-oneswt-assembly-" + version.value + ".jar" },
             oneswtAssemblyOutputPath in oneswtAssembly := { (target in oneswtAssembly).value / (oneswtAssemblyJarName in oneswtAssembly).value },
@@ -51,10 +53,23 @@ object OneSwt {
         // println(assemblyJar.getCanonicalPath)
         // println(s"out jar to: ${(oneswtAssemblyOutputPath in key).value}")
         val swtVersion = (version in key).value
+        val swtResolver = (oneswtResolver in key).value
+
+        val archs = Seq("cocoa-macosx-x86_64", "gtk-linux-x86", "gtk-linux-x86_64", "win32-win32-x86", "win32-win32-x86_64")
+        val swtModules: Seq[(String, File)] = archs map { arch =>
+            val artifactName = s"swt-$swtVersion-$arch"
+            val fileName = artifactName + ".jar"
+
+            val moduleId = "org.eclipse.swt" % artifactName % swtVersion
+            // TODO swtResolver에서 moduleId에 해당하는 dependency들 받아와서 실제 파일 위치 지정
+            (fileName, ???)
+        }
+
         OneSwt(
             out = (oneswtAssemblyOutputPath in key).value,
             assemblyFile = assemblyJar,
             swtVersion = swtVersion,
+            swtModules = swtModules,
             log = (streams in key).value.log
         )
     }
@@ -70,7 +85,7 @@ object OneSwt {
         }
     }
 
-    def apply(out: File, assemblyFile: File, swtVersion: String, log: Logger): File = {
+    def apply(out: File, assemblyFile: File, swtVersion: String, swtModules: Seq[(String, File)], log: Logger): File = {
         // 1. assemblyJar에서 org/eclipse/swt 폴더 삭제
         // 2. assemblyJar에 swt jar들 넣기
         // 3. SwtLoader에서 assemblyJar의 원래 메인 클래스 실행하도록 수정하기(manifest 이용)
@@ -115,9 +130,11 @@ object OneSwt {
             transfer(entryContent, outStream)
             outStream.closeEntry()
         }
-        // TODO - dependency manager에서 "com.giyeok.oneswt" % "swt-**" % swtVersion 에 해당하는 jar얻어오기
+        swtModules foreach { swtModule =>
+            addFile(swtModule._1, new FileInputStream(swtModule._2))
+        }
 
-        // TODO - SwtLoader 클래스 넣기
+        // SwtLoader 클래스 추가
         // addFile("/com/giyeok/oneswt/SwtLoader.class", ???)
 
         outStream.close()
